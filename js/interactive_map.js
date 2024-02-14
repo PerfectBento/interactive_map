fe8c84c6367208ccf22dd2cb6f766711d0da8812
PerfectBento <xazemage@yandex.ru>
2024-02-14 13:19:30 +0300

    Добавление 3 состояния значка номера в зависимости от занятости дома

--- js/interactive_map.js
+++ js/interactive_map.js
@@ -1,15 +1,41 @@
-const homes = document.querySelectorAll(".home");
+const homes_green = document.querySelectorAll(".home_green");
+const homes_yelow = document.querySelectorAll(".home_yelow");
+const homes_red = document.querySelectorAll(".home_red");
 const popup = document.querySelector(".popup_house");
+const number = document.querySelector(".number");
 const close = popup.querySelector(".close_block");
-homes.forEach(home => {
+homes_green.forEach(home => {
     home.addEventListener("click", function(){
     //    alert(this.dataset.title);
     popup.querySelector(".title").innerText = this.dataset.title;
     popup.classList.add("open");
+    number.classList.add("green");
+
+    });
+});
+
+homes_yelow.forEach(home => {
+    home.addEventListener("click", function(){
+    //    alert(this.dataset.title);
+    popup.querySelector(".title").innerText = this.dataset.title;
+    popup.classList.add("open");
+    number.classList.add("yellow");
+
+    });
+});
+homes_red.forEach(home => {
+    home.addEventListener("click", function(){
+    //    alert(this.dataset.title);
+    popup.querySelector(".title").innerText = this.dataset.title;
+    popup.classList.add("open");
+    number.classList.add("red");
 
     });
 });
 
 close.addEventListener("click", function(){
     popup.classList.remove("open");
+    number.classList.remove("red");
+    number.classList.remove("yellow");
+    number.classList.remove("green");
 });
